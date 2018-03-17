e6792874eee1470f112f6df0d7472ebabdce29b8
He-Wei Lee <b05705006@ntu.edu.tw>
2018-03-17 18:00:38 +0800

    customize hopscotch

--- js/tmplt.js
+++ js/tmplt.js
@@ -129,7 +129,7 @@ $(document).ready(function(){
 	$('#logout-btn').on('click', logout);
 
 	// Start the tour!
-	// hopscotch.startTour(tour);
+	hopscotch.startTour(tour);
 	// startIntro();
 });
 
