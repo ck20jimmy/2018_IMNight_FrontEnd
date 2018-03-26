e5cbb9ce939a6ad537468f52bd7c91a42bc30ed7
He-Wei Lee <b05705006@ntu.edu.tw>
2018-03-26 14:07:03 +0800

    fix menupage index

--- js/index.js
+++ js/index.js
@@ -1,76 +1,11 @@
-function lazyload() {
-    $('.lazy').Lazy({
-        effect: 'fadeIn',
-        effectTime: 500,
-        threshold: 0,
-        onError: function(element) {
-            console.log('error loading ' + element.data('src'));
-        }
-    });   
-}
-
 $(function(){
     $('#home-word').fadeIn(1000, 'swing', function() {
-        $('#start').show("blind", 500, lazyload);
+        $('#start').show("blind", 500);
     });
-
-    for(let i = 1; i <= 4; i++) {
-        $('#logo' + i).on('load', function(){
-            $('#intro' + i).animate({'opacity':'1'},500);                     
-        });
-    }
 })
 
 $('#start').on('click', function() {
     setTimeout(function(){
         location.href = "home.html";
     }, 500);
-});    
-
-// function fadeInImg(obj) {
-//     obj.animate({'opacity':'1'},1500);
-// }
-
-// function fadeInImgInit(obj) {
-//     // for fade in on seen
-//     $('.hideme').each(function() {
-//         let window_bot = $(window).scrollTop() + $(window).height();
-//         let obj_top = $(this).offset().top;
-//         if (window_bot > obj_top) {
-//             fadeInImg($(this));         
-//         }
-//     })
-// }
-
-// $(window).on('load', function() {
-//     // for lazy load
-//     // $('.lazy').lazy();
-
-//  $('#home-word').fadeIn(1000, 'swing', function() {
-//      $('#start').show("blind", 500, fadeInImgInit);
-//  });
-
-//     // /* Every time the window is scrolled ... */
-//     // $(window).scroll( function(){
-//     //     /* Check the location of each desired element */
-//     //     $('.hideme').each( function(i){
-//     //         var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-//     //         var top_of_object = $(this).offset().top;
-//     //         var bottom_of_window = $(window).scrollTop() + $(window).height();
-//     //         var top_of_window = $(window).scrollTop();
-
-//     //         if( bottom_of_window > top_of_object ){
-//     //             // $(this).animate({'opacity':'1'},1500);
-//     //             fadeInImg($(this));
-//     //         }
-//     //     }); 
-//     // }); 
-// });
-
-// $('.enlarge').hover(
-// function(){
-//  $(this).addClass('transition');
-// }, 
-// function(){
-//  $(this).removeClass('transition');
-// });
+});
--- js/menuPage.js
+++ js/menuPage.js
@@ -4,12 +4,10 @@ function imgEnlarge() {
 		$('.enlarge').hover(
 		function(){
 			$(this).css('cursor', 'pointer');
-			// $(this).addClass('transition');
 			$(this).css('transform', 'scale(1.1)');
 			$(this).removeClass('floating');
 		}, 
 		function(){
-			// $(this).removeClass('transition');
 			$(this).addClass('floating');
 		});
 	}, 1000);
@@ -32,6 +30,8 @@ $(function(){
 	$('#rule-title').fadeTo(1000, 0.85, 'swing', function() {
 		$('#rule-space').show("blind", 800);
 	});
+
+	$('[data-toggle="tooltip"]').tooltip({'placement': 'top'});
 
 	// add enlarge hover effect on all .floating elements
 	imgEnlarge();
